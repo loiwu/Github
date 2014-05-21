8b10e204b14fa9453daa0eb75b43de73de24ede6
Loi Wu <superloiwu@gmail.com>
2014-05-21 13:59:38 +0800

    NETproj

---
+++ BAEN/BAEN/BAEN/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BAEN
+//
+//  Created by loi on 5/21/14.
+//  Copyright (c) 2014 GWrabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
