d9b966cc63951fce5aa79f3cd00c981e570f3d7f
Loi Wu <superloiwu@gmail.com>
2014-05-21 11:01:11 +0800

    NETKit

---
+++ BAEN/GWNEWKit/GWNEWKit/GWNEWKit.h
@@ -0,0 +1,13 @@
+//
+//  GWNEWKit.h
+//  GWNEWKit
+//
+//  Created by loi on 5/21/14.
+//  Copyright (c) 2014 GWrabbit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GWNEWKit : NSObject
+
+@end
---
+++ BAEN/GWNEWKit/GWNEWKit/GWNEWKit.m
@@ -0,0 +1,13 @@
+//
+//  GWNEWKit.m
+//  GWNEWKit
+//
+//  Created by loi on 5/21/14.
+//  Copyright (c) 2014 GWrabbit. All rights reserved.
+//
+
+#import "GWNEWKit.h"
+
+@implementation GWNEWKit
+
+@end
