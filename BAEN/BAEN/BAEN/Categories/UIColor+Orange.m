1bcf86483221da3c5ba02a35af8b17e8170a2986
Loi Wu <superloiwu@gmail.com>
2014-05-22 10:26:32 +0800

    Initial Commit

--- BAEN/BAEN/BAEN/Categories/UIColor+Orange.m
+++ BAEN/BAEN/BAEN/Categories/UIColor+Orange.m
@@ -6,20 +6,22 @@
 //  Copyright (c) 2014 GWrabbit. All rights reserved.
 //
 
+// -loi- // Control the main UI dominant tone - choose blue
+
 #import "UIColor+Orange.h"
 
 @implementation UIColor (Orange)
 
 + (UIColor *)mainOrangeColor {
-    return [UIColor colorWithRed:1.0f green:0.4f blue:0.0f alpha:1.0f];
+    return [UIColor colorWithRed:(65.0f / 255.0f) green:(105.0f / 255.0f) blue:(225.0f / 255.0f) alpha:1.0f];
 }
 
 + (UIColor *)lightOrangeColor {
-    return [UIColor colorWithRed:1.0f green:0.6f blue:0.2f alpha:1.0f];
+    return [UIColor colorWithRed:(30.0f / 255.0f) green:(144.0f / 255.0f) blue:(255.0f / 255.0f) alpha:1.0f];
 }
 
 + (UIColor *)paleOrangeColor {
-    return [UIColor colorWithRed:(234.0f / 255.0f) green:(232.0f / 255.0f) blue:(224.0f / 255.0f) alpha:1.0f];
+    return [UIColor colorWithRed:(176.0f / 255.0f) green:(196.0f / 255.0f) blue:(222.0f / 255.0f) alpha:1.0f];
 }
 
 @end
---
+++ BAEN/iBAEN/iBAEN/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBAEN
+//
+//  Created by loi on 5/22/14.
+//  Copyright (c) 2014 GWrabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
